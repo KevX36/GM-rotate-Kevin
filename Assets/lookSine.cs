cbab308e124b57ab2e94f4bb7121c2a52c40a741
KevX36 <kevincard2007@gmail.com>
2025-11-06 15:15:46 -0400

    finished the last 2 need to make the rotation correct for the 4th part

--- Assets/lookSine.cs
+++ Assets/lookSine.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class lookSine : MonoBehaviour
@@ -5,9 +6,16 @@ public class lookSine : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        start = transform.position;
+        far = transform.position;
+        start.z += 5;
+        far.z -= 5;
     }
-
+    public GameObject cube;
+    private float timer = 0.0f;
+    Vector3 start;
+    Vector3 far;
+    private bool MadeItToFar = false;
     // Update is called once per frame
     void Update()
     {
@@ -18,11 +26,33 @@ public class lookSine : MonoBehaviour
 
         transform.rotation = rotate;
 
-
-
-
-
-
+        timer += Time.deltaTime;
+        if (timer >= 10)
+        {
+            timer = 0;
+            if (MadeItToFar == false)
+            {
+                MadeItToFar = (true);
+            }
+            else if (MadeItToFar == true)
+            {
+                MadeItToFar = (false);
+            }
+        }
         
+        if (MadeItToFar == false)
+        {
+            cube.transform.position = Vector3.Slerp(start, far, timer / 10);
+            
+        }
+        if (MadeItToFar == true)
+        {
+            cube.transform.position = Vector3.Slerp(far, start, timer / 10);
+            
+        }
+
+
+
+
     }
 }

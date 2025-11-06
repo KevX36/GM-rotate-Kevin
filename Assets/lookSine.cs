a36ed648f173af9dcae3e87ef9a38ff2a845a02c
KevX36 <kevincard2007@gmail.com>
2025-11-06 19:19:18 -0400

    after a LOT of trial and error finished 4

--- Assets/lookSine.cs
+++ Assets/lookSine.cs
@@ -6,36 +6,26 @@ public class lookSine : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        start = transform.position;
-        far = transform.position;
-        start.z += 5;
-        far.z -= 5;
+        
     }
     public GameObject cube;
-    private float timer = 0.0f;
-    Vector3 start;
-    Vector3 far;
-    private bool MadeItToFar = false;
+    
     // Update is called once per frame
     void Update()
     {
-        Quaternion rotate = transform.rotation;
+        Vector3 rotate = transform.eulerAngles;
+        rotate.y +=0.1f;
 
-        rotate.y = Mathf.Sin(Time.time);
-        rotate.y /= 2;
+        
 
-        transform.rotation = rotate;
+        transform.eulerAngles = rotate;
 
-        timer += Time.deltaTime;
-        if (timer >= 10)
-        {
-            timer = 0;
-            
-        }
+        Quaternion cubeCurrent = cube.transform.rotation;
+        Quaternion cubeTarget = transform.rotation;
+        cube.transform.rotation = Quaternion.RotateTowards(cubeCurrent, cubeTarget, 1);
+        
 
-        cube.transform.position = Vector3.Slerp(start, far, timer / 10);
-
-
+        cube.transform.Translate(0, 0, 0.01f);
 
 
     }
